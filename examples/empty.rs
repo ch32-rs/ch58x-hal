997b7e8876dc3673afbe2d352d8493e92d8f646d
Andelf <andelf@gmail.com>
2023-10-20 11:57:54 +0800

    chore: refine rtc

---
+++ examples/empty.rs
@@ -0,0 +1,64 @@
+#![no_std]
+#![no_main]
+
+use embedded_hal_alpha::delay::DelayUs;
+use hal::gpio::{Input, Level, Output, OutputDrive, Pull};
+use hal::peripherals;
+use hal::systick::SysTick;
+use hal::uart::UartTx;
+use {ch58x_hal as hal, panic_halt as _};
+// use hal::interrupt::Interrupt;
+// use hal::rtc::{DateTime, Rtc};
+
+static mut SERIAL: Option<UartTx<peripherals::UART1>> = None;
+
+macro_rules! println {
+    ($($arg:tt)*) => {
+        unsafe {
+            use core::fmt::Write;
+            use core::writeln;
+
+            if let Some(uart) = SERIAL.as_mut() {
+                writeln!(uart, $($arg)*).unwrap();
+            }
+        }
+    }
+}
+
+#[ch32v_rt::entry]
+fn main() -> ! {
+    // LED PA8
+
+    let mut config = hal::Config::default();
+    config.clock.use_pll_80mhz();
+    let p = hal::init(config);
+
+    let mut delay = SysTick::new(p.SYSTICK);
+
+    let mut led = Output::new(p.PA8, Level::Low, OutputDrive::Low);
+
+    let download_button = Input::new(p.PB22, Pull::Up);
+    let reset_button = Input::new(p.PB23, Pull::Up);
+
+    let uart = UartTx::new(p.UART1, p.PA9, Default::default()).unwrap();
+    unsafe {
+        SERIAL.replace(uart);
+    }
+
+    println!("\nHello World!");
+    println!("Clocks: {}", hal::sysctl::clocks().hclk);
+    println!("ChipID: {:02x}", hal::signature::get_chip_id());
+
+    loop {
+        println!("tick");
+        led.toggle();
+
+        delay.delay_ms(1000);
+
+        while download_button.is_low() {}
+
+        if reset_button.is_low() {
+            println!("button: {} {}", download_button.is_low(), reset_button.is_low());
+        }
+    }
+}
